fa4296d781fa9d3ad1ea9bae181b2802f7646d9a
ChoudharyUpen <chaudharyuday1515@gmail.com>
2026-07-25 06:49:59 +0530

    Create 17.Chocolate Feast.java

---
+++ 17.Chocolate Feast.java
@@ -0,0 +1,31 @@
+pattern ----
+
+
+
+
+
+status ----
+i have written code but saflta nahi mili 
+
+
+
+
+my code ----
+class Result {
+    public static int chocolateFeast(int n, int c, int m) {
+    int a =n%c;
+    int b=a;
+    while(a >0){
+        if(a < m ){
+            return b;
+        }
+        else if (a ==m ){
+            b++;
+            return b ;
+        }
+    }
+
+    }
+
+}
+
