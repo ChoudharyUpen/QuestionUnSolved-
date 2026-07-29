1c1cae57ac29e7db954ea903fe858b73f465e435
ChoudharyUpen <chaudharyuday1515@gmail.com>
2026-07-29 08:15:21 +0530

    Create 24.Manasa  & Stones .java

---
+++ 24.Manasa  & Stones .java
@@ -0,0 +1,6 @@
+pattern --- i think this question is from set theory (permuation ,combiantion )
+
+
+
+
+status ---  could not do it because so much time spend in understanding the question and also because this question is from set theory i have to make it more strong 
